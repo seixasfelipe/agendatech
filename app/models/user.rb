8b16e16981c07b4f40a57e66542e059f96d39d13
asouza <alots.ssa@gmail.com>
2011-02-25 00:12:50 -0300

    pegando os eventos que o usuario partipou

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,22 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :nickname, :image
-  
-  
+
+
   #tem alguma diferenca entre criar o scope e o metodo para a busca. No scope nao tava retornando nil...
   module Scopes
     def por_login_social(nick)
       where("nickname = ?", nick).first
     end
   end
-  
+
   extend Scopes
 
+  def eventos_que_vai_ou_foi
+    Evento.joins(:gadgets).where("gadgets.user_id=?", self.id).where("gadgets.tipo=?", Gadget.tipos[:eu_vou]).ordenado_por_data
+  end
+
   def vai_no? evento
-    return  Gadget.where(:evento_id => evento.id, :user_id => self.id).exists?
+    return Gadget.where(:evento_id => evento.id, :user_id => self.id).exists?
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,16 +5,25 @@ describe Grupo do
   describe "vai no evento" do
     before(:all) do
       @user = User.create :nickname => "teste",:email => "teste@teste.com.br",:image => "http://a3.twimg.com/profile_images/1201901056/ic_launcher.png"
-      @evento = Evento.create :nome => "evento", :descricao => "desc", :site => "http://www.example.com", :data => "10/09/2020", :estado => 'BA',:aprovado => true      
+      @evento1 = Evento.create :nome => "evento", :descricao => "desc", :site => "http://www.example.com", :data => "10/09/2020", :estado => 'BA',:aprovado => true
     end
     
     it "deveria retornar false caso nao fosse" do
-      @user.vai_no?(@evento).should be_false      
+      @user.vai_no?(@evento1).should be_false      
     end
     
     it "deveria retornar true caso fosse" do
-      Gadget.create :tipo => Gadget.tipos[:eu_vou], :evento_id => @evento.id, :user_id => @user.id      
-      @user.vai_no?(@evento).should be_true      
-    end    
+      Gadget.create :tipo => Gadget.tipos[:eu_vou], :evento_id => @evento1.id, :user_id => @user.id
+      @user.vai_no?(@evento1).should be_true      
+    end
+
+    it "deveria retornar os eventos que vai ou foi" do
+      @evento2 = Evento.create :nome => "evento", :descricao => "desc", :site => "http://www.example.com", :data => "10/10/2020", :estado => 'BA',:aprovado => true
+      @evento3 = Evento.create :nome => "evento", :descricao => "desc", :site => "http://www.example.com", :data => "10/10/2020", :estado => 'BA',:aprovado => true      
+      Gadget.create :tipo => Gadget.tipos[:eu_vou], :evento_id => @evento1.id, :user_id => @user.id
+      Gadget.create :tipo => Gadget.tipos[:eu_vou], :evento_id => @evento2.id, :user_id => @user.id
+      @user.eventos_que_vai_ou_foi.size.should == 2
+      @user.eventos_que_vai_ou_foi[0].should == @evento1
+    end
   end
 end
