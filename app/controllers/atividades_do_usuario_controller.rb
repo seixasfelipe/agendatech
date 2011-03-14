47dae8962bd968aca6a96c2c7a9ccba661496dc8
andersonleite <andersonlfl@gmail.com>
2011-03-14 16:31:03 -0300

    comentarios no profile

--- app/controllers/atividades_do_usuario_controller.rb
+++ app/controllers/atividades_do_usuario_controller.rb
@@ -1,5 +1,6 @@
 class AtividadesDoUsuarioController < ApplicationController
   def index
     @usuario = User.por_login_social params[:nick]
+    @comentarios = Comentario.where('twitter = ?', [params[:nick]])
   end
 end
